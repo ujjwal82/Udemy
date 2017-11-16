a19000389e103e56a550b35ae8c02624e01f8609
Ujjwal Kumar <ujjwal82@gmail.com>
2017-11-16 08:56:21 +0530

    Commited random forest regression

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/Practice/decision_tree_regression.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/Practice/decision_tree_regression.R
@@ -1,4 +1,3 @@
-# Decision Tree Regression
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
@@ -16,17 +15,31 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Decision Tree Regression to the dataset
+# Fitting the Regression Model to the dataset
 # install.packages('rpart')
 library(rpart)
-regressor = rpart(formula = Salary ~ .,
-                  data = dataset,
-                  control = rpart.control(minsplit = 1))
+regressor <- rpart( formula = Salary ~ .,
+                    data = dataset,
+                    control = rpart.control(minsplit = 1))
 
-# Predicting a new result with Decision Tree Regression
+
+# Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Decision Tree Regression results (higher resolution)
+# Visualising the Regression Model results
+# install.packages('ggplot2')
+# install.packages('colorspace')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Decision Tree Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualising the Regression Model results (for higher resolution and smoother curve)
 # install.packages('ggplot2')
 library(ggplot2)
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
@@ -38,7 +51,3 @@ ggplot() +
   ggtitle('Truth or Bluff (Decision Tree Regression)') +
   xlab('Level') +
   ylab('Salary')
-
-# Plotting the tree
-plot(regressor)
-text(regressor)
